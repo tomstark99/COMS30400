3531c01f9bb9b6c7d1cc5fb66456af743796ddc4
Omar Suleiman <jb18940@bristol.ac.uk>
2021-02-05 14:26:11 +0100

    template.unity: added material to character

--- Freight/Assets/Scripts/PlayerMovement.cs
+++ Freight/Assets/Scripts/PlayerMovement.cs
@@ -10,8 +10,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //falo
-        //falo
+        
     }
 
     // Update is called once per frame
