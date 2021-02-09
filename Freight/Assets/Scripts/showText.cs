bbb4d2c85822a071f06e0d2e9ce9c28dd07fcf49
dANW34V3R <dan.weaver@hotmail.co.uk>
2021-02-09 17:01:39 +0000

    PoseEstimation: Pass pose data into unity and display in text box

---
+++ Freight/Assets/Scripts/showText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+
+public class showText : MonoBehaviour
+{
+
+    [DllImport("__Internal")]
+    private static extern string StringReturnValueFunction();
+
+    private Text txtMy;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtMy = GameObject.Find("Canvas/Text").GetComponent<Text>();
+        txtMy.text = "This text";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            
+            string pose = StringReturnValueFunction();
+
+            txtMy.text = pose;
+
+            // GetComponent(TextMesh).text = pose;
+        }
+    }
+}
