48bea965129c6cddf21e0c9eafcb3a444186b68a
Omar Suleiman <jb18940@bristol.ac.uk>
2021-02-11 22:36:47 +0100

    When 2 seperate clients connect, they see a seperate guard each but both guards are in scene

---
+++ Freight/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    public List<Transform> players = new List<Transform>();
+
+    List<Transform> GetPlayers()
+    {
+        GameObject[] temp = GameObject.FindGameObjectsWithTag("Player");
+        List<Transform> temp_players = new List<Transform>();
+        foreach (GameObject t in temp)
+        {
+            temp_players.Add(t.transform);
+        }
+        return temp_players;
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        players = GetPlayers();
+    }
+}
