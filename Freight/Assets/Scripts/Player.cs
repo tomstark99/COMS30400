f7f7b75026507efb7aa602c9a17151db91676b3d
Omar Suleiman <jb18940@bristol.ac.uk>
2021-02-16 15:34:38 +0100

    MyNetworkManager: made new network manager script that extends default Former-commit-id: 23378b8191a896f73cd0cbf7f73e705adb33f4a5

---
+++ Freight/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    private MyNetworkManager room;
+    private MyNetworkManager Room
+    {
+        get
+        {
+            if (room != null) { return room; }
+            return room = NetworkManager.singleton as MyNetworkManager;
+        }
+    }
+
+    public override void OnStartClient()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        Room.GamePlayers.Add(this);
+    }
+
+    public override void OnStopClient()
+    {
+        Room.GamePlayers.Remove(this);
+    }
+}
