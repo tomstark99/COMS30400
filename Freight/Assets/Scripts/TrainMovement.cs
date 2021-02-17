35f25dc38bda3183ab1492373811ac304e85ba61
tomstark99 <jb18789@bristol.ac.uk>
2021-02-17 16:53:06 +0000

    Fixed train movement
    Movement waypoints were deleted, fixed to make simple movement, no curve anymore.
    
    Added back the engine shed
    
    Former-commit-id: 74a14bc529fcbdae0147a32cc81d079bb70414ca

--- Freight/Assets/Scripts/TrainMovement.cs
+++ Freight/Assets/Scripts/TrainMovement.cs
@@ -18,12 +18,12 @@ public class TrainMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position != target[current].position && transform.rotation != target[current].rotation) {
+        if(transform.position != target[current].position) {
             Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
             Vector3 rot = Vector3.RotateTowards(transform.forward, (transform.position - target[current].position), rotationSpeed * Time.deltaTime, 0.0f);
-            transform.position = pos;
-            transform.rotation = Quaternion.LookRotation(rot);
-            // GetComponent<Rigidbody>().MovePosition(pos);
+            // transform.position = pos;
+            // transform.rotation = Quaternion.LookRotation(rot);
+            GetComponent<Rigidbody>().MovePosition(pos);
             // GetComponent<Rigidbody>().RotatePosition(rot);
         } else {
             current = (current + 1) % target.Length;
