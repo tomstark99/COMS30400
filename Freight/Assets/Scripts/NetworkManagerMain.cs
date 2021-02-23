d9b7c1054c5a9f7e0340d622c31eaf019a7f2b98
Omar Suleiman <jb18940@bristol.ac.uk>
2021-02-23 16:55:07 +0100

    NetworkManager: Removed useless code

--- Freight/Assets/Scripts/NetworkManagerMain.cs
+++ Freight/Assets/Scripts/NetworkManagerMain.cs
@@ -10,17 +10,9 @@ public class NetworkManagerMain : NetworkManager
 {
     [Scene] [SerializeField] private string menuScene = string.Empty;
 
-
- 
-
     public override void OnStartServer()
     {
         //spawnPrefabs = Resources.LoadAll<GameObject>("Prefabs/ModelPrefabs").ToList();
-        //seed = (int)DateTime.Now.Ticks;
-        //UnityEngine.Random.InitState(seed);
-        //SpawnTrees();
-        //SpawnTrains();
-        //surface.BuildNavMesh();
     }
 
     public List<Player> GamePlayers { get; } = new List<Player>();
@@ -28,9 +20,6 @@ public class NetworkManagerMain : NetworkManager
     public override void OnStartClient()
     {
         //var spawnablePrefabs = Resources.LoadAll<GameObject>("Prefabs/ModelPrefabs");
-        //UnityEngine.Random.InitState(seed);
-        //SpawnTrees();
-        //SpawnTrains();
         //foreach (var prefab in spawnablePrefabs)
         //{
         //    Debug.Log(prefab);
@@ -38,14 +27,8 @@ public class NetworkManagerMain : NetworkManager
         //}
     }
 
-    public override void OnClientDisconnect(NetworkConnection conn)
-    {
-        //GamePlayers.Remove();
-        base.OnClientDisconnect(conn);
-    }
-
     private void Update()
     {
-        //Debug.Log(GamePlayers.Count);
+
     }
 }
