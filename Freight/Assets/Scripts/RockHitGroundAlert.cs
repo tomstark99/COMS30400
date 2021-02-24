eda3a319d1b0f33f786b67d7907591892996a6b2
Omar Suleiman <jb18940@bristol.ac.uk>
2021-02-24 18:12:43 +0100

    Guard + Rock: guards get alerted when rock hits ground next to a guard
    Former-commit-id: cb30a40471bb473567f8424a6777e304e84417bc

--- Freight/Assets/Scripts/RockHitGroundAlert.cs
+++ Freight/Assets/Scripts/RockHitGroundAlert.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class RockHitGroundAlert : MonoBehaviour
+public class RockHitGroundAlert : NetworkBehaviour
 {
     public Transform groundCheck;
     private GameObject guards;
@@ -10,7 +11,9 @@ public class RockHitGroundAlert : MonoBehaviour
     public LayerMask groundMask;
     private bool isGrounded ;
     private bool lastFrameValueOfIsGrounded;
+    [SyncVar] public bool rockHitGround;
 
+    [ServerCallback]
     // Start is called before the first frame update
     void Start()
     {
@@ -27,12 +30,21 @@ public class RockHitGroundAlert : MonoBehaviour
         }
     }
 
+    [ServerCallback]
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if (isGrounded == true && lastFrameValueOfIsGrounded == false)
+        {
             Debug.Log("Rock hit the ground alie");
+            rockHitGround = true;
+        }
+        else
+        {
+            rockHitGround = false;
+        }
+
         lastFrameValueOfIsGrounded = isGrounded;
     }
    
