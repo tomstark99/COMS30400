79ab37b865a5d863709f15be293dde8780a07665
Stefan Pryuna <mc18112@bristol.ac.uk>
2021-02-26 12:20:35 +0200

    how do you even have text to only display when close to the rock alie

---
+++ Freight/Assets/Scripts/VisibleByDistance.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+ public class VisibleByDistance : MonoBehaviour
+ {
+     public Transform PlayerObject;
+     public float DistanceToView = 10f;
+     private TextMesh myTextMesh;
+     private string MyOriginalText;
+ 
+     void Start()
+     {
+         myTextMesh = GetComponent<TextMesh>();
+         MyOriginalText = myTextMesh.text;
+     }
+     void Update()
+     {
+        /* if (Vector3.Distance(PlayerObject.position, transform.position) <= DistanceToView)
+         {
+             myTextMesh.text = MyOriginalText;
+         }
+         else
+         {
+             myTextMesh.text = "";
+         }*/
+     }
+ }
