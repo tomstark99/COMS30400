655f69a526cbd2a2689be0270b88505170955e6d
Omar Suleiman <jb18940@bristol.ac.uk>
2021-02-28 12:43:07 +0100

    got fence to pull apart when press Q, need to add pose recognition
    Former-commit-id: 929cb278277122db8677741b509741eab8184e32

---
+++ Freight/Assets/BreakFence.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class BreakFence : NetworkBehaviour
+{
+    public GameObject brokenFencePrefab;
+    private List<Player> players;
+    private NetworkManagerMain room;
+    public GameObject text;
+
+    private NetworkManagerMain Room
+    {
+        get
+        {
+            if (room != null) { return room; }
+            return room = NetworkManager.singleton as NetworkManagerMain;
+        }
+    }
+
+    [ServerCallback]
+    void Start()
+    {
+        players = Room.GamePlayers;
+    }
+
+    [TargetRpc]
+    void SetPressQToActive(NetworkConnection conn)
+    {
+        text.SetActive(true);
+    }
+
+    [TargetRpc]
+    void SetPressQToNotActive(NetworkConnection conn)
+    {
+        text.SetActive(false);
+    }
+
+    void Update()
+    {
+        players = Room.GamePlayers;
+
+        foreach (Player player in players)
+        {
+            float tempDist = Vector3.Distance(player.transform.position, transform.position);
+            if (tempDist <= 2.5f)
+            {
+                SetPressQToActive(player.connectionToClient);
+            }
+            else if (tempDist > 2.5f)
+            {
+                SetPressQToNotActive(player.connectionToClient);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            float dist = Vector3.Distance(NetworkClient.connection.identity.transform.position, transform.position);
+            if (dist <= 2.5f)
+            {
+                Vector3 spawnPosition = transform.position;
+                Destroy(transform.gameObject);
+                GameObject brokenFence = Instantiate(brokenFencePrefab, spawnPosition, Quaternion.Euler(0f, 90f, 0f));
+                NetworkServer.Spawn(brokenFence);
+            }
+        }
+    }
+
+}
+
