d985931949f118c0d20f7674b0e8eee94742cec1
Dan <dan.weaver@hotmail.co.uk>
2021-02-28 12:00:35 +0000

    Player: Add crouch and lying sideways
    Former-commit-id: 7e4869b206103ab13d838ab478975c94a1241c6e

---
+++ Freight/Assets/Scripts/PlayerGestureController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGestureController : MonoBehaviour
+{
+    public CharacterController character;
+    public Transform characterTransform;
+    
+    void Start()
+    {
+        character = gameObject.GetComponent<CharacterController>();
+        characterTransform = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            character.height = 1.0f;
+        }
+        else if (Input.GetKeyUp(KeyCode.C))
+        {
+            character.height = 1.8f;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            characterTransform.rotation =  Quaternion.Euler(0, 0, 90);
+        }
+        else if (Input.GetKeyUp(KeyCode.Q))
+        {
+            characterTransform.rotation =  Quaternion.Euler(0, 0, 0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            characterTransform.rotation =  Quaternion.Euler(0, 0, -90);
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            characterTransform.rotation =  Quaternion.Euler(0, 0, 0);
+        }
+        
+    }
+}
