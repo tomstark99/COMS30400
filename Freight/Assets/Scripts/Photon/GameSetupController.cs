4bbd10dab991b9e433aa27f406647213b5125446
Stefan Pryuna <mc18112@bristol.ac.uk>
2021-03-02 17:58:59 +0200

    GameSetupController
    Former-commit-id: 10044375d9f106a673546b88fbc2e52485c32d42

---
+++ Freight/Assets/Scripts/Photon/GameSetupController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+public class GameSetupController : MonoBehaviour
+{
+    /*
+     * All this script is doing in the end is creating the player object. The game is told to find this player object under a 
+     * PhotonPrefabs folder and to look for the prefab named PhotonPlayer.
+     * Everything is setting the starting position and rotation values.
+     * */
+     void Start()
+    {
+        CreatePlayer();
+    }
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating Player");
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs",
+                "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+    }
+}
