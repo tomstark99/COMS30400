f8aaeaaf0d998ed5fbb7cd2e64badcaa573470dd
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-02 22:01:28 +0100

    pickup rock :3
    Former-commit-id: 77bb7f3de748ab3386edc14fe63a1fccb5af26cb

---
+++ Freight/Assets/Scripts/Photon/ItemPickUpPhoton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ItemPickUpPhoton : MonoBehaviourPun
+{
+    public GameObject rocks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocks = GameObject.Find("Rocks");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rocks = GameObject.Find("Rocks");
+
+        foreach (Transform rock in rocks.transform)
+        {
+            float tempDist = Vector3.Distance(transform.position, rock.position);
+            if (tempDist <= 5f && Input.GetKeyDown(KeyCode.F))
+            {
+                rock.parent = transform.GetChild(3);
+                rock.GetComponent<Rigidbody>().isKinematic = true;
+                rock.GetComponent<Rigidbody>().useGravity = false;
+                rock.transform.position = rock.parent.position;
+            }
+        }
+    }
+}
