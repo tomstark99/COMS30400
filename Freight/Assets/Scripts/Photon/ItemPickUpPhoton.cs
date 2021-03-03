f07f3d535ed6c39e54baba3f82185c5b31ea5859
Stefan Pryuna <mc18112@bristol.ac.uk>
2021-03-03 12:20:46 +0200

    Rock Pick up seen by clients PunRPC function

--- Freight/Assets/Scripts/Photon/ItemPickUpPhoton.cs
+++ Freight/Assets/Scripts/Photon/ItemPickUpPhoton.cs
@@ -2,32 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-
+using System.IO;
 public class ItemPickUpPhoton : MonoBehaviourPun
 {
     public GameObject rocks;
-
+    public GameObject rockPrefab;
+    private GameObject rock;
     // Start is called before the first frame update
     void Start()
     {
+
         rocks = GameObject.Find("Rocks");
+        rock = rocks.transform.GetChild(0).gameObject;
     }
+
 
     // Update is called once per frame
     void Update()
     {
         rocks = GameObject.Find("Rocks");
 
-        foreach (Transform rock in rocks.transform)
+       
+
+        if(Input.GetKeyDown(KeyCode.E) && photonView.IsMine)
         {
-            float tempDist = Vector3.Distance(transform.position, rock.position);
-            if (tempDist <= 5f && Input.GetKeyDown(KeyCode.F))
-            {
-                rock.parent = transform.GetChild(3);
-                rock.GetComponent<Rigidbody>().isKinematic = true;
-                rock.GetComponent<Rigidbody>().useGravity = false;
-                rock.transform.position = rock.parent.position;
-            }
+            photonView.RPC("PickUpRock", RpcTarget.All);
+       
+
         }
     }
+
+    [PunRPC]
+    void PickUpRock()
+    {
+        
+        rock.transform.parent = transform.GetChild(3);
+        rock.GetComponent<Rigidbody>().isKinematic = true;
+        rock.transform.position = rock.transform.parent.position;
+    }
+
+    
 }
