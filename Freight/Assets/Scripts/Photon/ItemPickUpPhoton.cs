a73d834d44779fca2ecd92bf44674f366a0f4525
Stefan Pryuna <mc18112@bristol.ac.uk>
2021-03-03 12:40:29 +0200

    ape code but rock throw works

--- Freight/Assets/Scripts/Photon/ItemPickUpPhoton.cs
+++ Freight/Assets/Scripts/Photon/ItemPickUpPhoton.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using System.IO;
+
 public class ItemPickUpPhoton : MonoBehaviourPun
 {
     public GameObject rocks;
     public GameObject rockPrefab;
     private GameObject rock;
+
+    GameObject cube;
+    GameObject camera;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +34,17 @@ public class ItemPickUpPhoton : MonoBehaviourPun
        
 
         }
+
+        if(Input.GetMouseButtonDown(0) && photonView.IsMine)
+        {
+           
+
+             cube = transform.GetChild(2).gameObject;
+
+             camera = cube.transform.GetChild(0).gameObject;
+
+            photonView.RPC("ThrowRock", RpcTarget.All);
+        }
     }
 
     [PunRPC]
@@ -39,6 +54,14 @@ public class ItemPickUpPhoton : MonoBehaviourPun
         rock.transform.parent = transform.GetChild(3);
         rock.GetComponent<Rigidbody>().isKinematic = true;
         rock.transform.position = rock.transform.parent.position;
+    }
+
+    [PunRPC]
+    void ThrowRock()
+    {
+        rock.transform.parent = rocks.transform;
+        rock.GetComponent<Rigidbody>().isKinematic = false;
+        rock.GetComponent<Rigidbody>().AddForce(camera.transform.forward * 1000);
     }
 
     
