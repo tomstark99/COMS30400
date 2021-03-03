2e2d962da94afb0e356d5d75a19e41710d745f92
Stefan Pryuna <mc18112@bristol.ac.uk>
2021-03-03 17:26:40 +0200

    git cloning repo like there s no tommorow ngl
    Former-commit-id: 1f39f10c7edad68d878b96a070208d0a1281d92c

---
+++ Freight/Assets/Scripts/Photon/Character.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    public Transform pickUpDestination;
+    public PickUpable currentHeldItem;
+    
+    public void PickUp(PickUpable Item) {
+        currentHeldItem = item;
+
+        // An item can only be moved by a player if they are the owner.
+        // Therefore, give ownership of the item to the local player before
+        // moving it.
+        PhotonView view = item.GetComponent<PhotonView>();
+        view.TransferOwnership(PhotonNetwork.LocalPlayer);
+        item.SetItemPickupConditions();
+
+        // Move to players pickup destination.
+        item.transform.position = pickupDestination.position;
+
+        // Set the parent of the object to the pickupDestination so that it moves
+        // with the player.
+        item.transform.parent = pickupDestination;
+    }
+
+    public void PutDown(PickUpable item) {
+
+        currentHeldItem = null;
+        item.ResetItemConditions(this);
+
+        item.transform.parent = GameObject.Find("/Environment/Interactables").transform;
+  }
+
+   public virtual Vector3 Velocity() {
+       
+    return GetComponent<CharacterController>().velocity;
+    }
+}
