188a403d973833e2fe9a91a02dca29728c3284b9
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-08 18:44:22 +0100

    map and timetable appear when press m and t :DDDDD

--- Freight/Assets/Scripts/Photon/PhotonPlayer.cs
+++ Freight/Assets/Scripts/Photon/PhotonPlayer.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class PhotonPlayer : MonoBehaviour
+public class PhotonPlayer : MonoBehaviourPun
 {
+    public GameObject playerUI;
+    private GameObject uiRef;
+    public string gesture;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiRef = Instantiate(playerUI);
+        uiRef.transform.parent = gameObject.transform;
+        gesture = PoseParser.GETGestureAsString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+        string tempGesture = PoseParser.GETGestureAsString();
+        if (tempGesture != gesture)
+        {
+            gesture = tempGesture;
+        }
+        Debug.Log(gesture);
     }
 }
