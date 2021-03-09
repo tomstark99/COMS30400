59971c89623f7ac87fcd20d6c342962896a72601
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-09 16:48:35 +0100

    added script to create room

---
+++ Freight/Assets/Scripts/Photon/CreateRoomMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class CreateRoomMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private TextMeshProUGUI roomName;
+
+    public void OnClick_CreateRoom()
+    {
+        if (!PhotonNetwork.IsConnected) return;
+
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom(roomName.text, options, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Room created");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room creation failed " + message);
+    }
+}
