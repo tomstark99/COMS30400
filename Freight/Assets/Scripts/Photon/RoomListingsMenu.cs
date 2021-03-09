209fdf6edb6e24d8ecc6deafd615eb7e347a9875
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-09 17:12:44 +0100

    think lobbies show up
    Former-commit-id: 5f351f17b859243f9a29e2debec9a926d6167ed6

---
+++ Freight/Assets/Scripts/Photon/RoomListingsMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomListingsMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Transform content;
+    [SerializeField]
+    private RoomListing roomListing;
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (RoomInfo info in roomList)
+        {
+            RoomListing listing = Instantiate(roomListing, content);
+            if (listing != null)
+            {
+                listing.SetRoomInfo(info);
+            }
+        }
+    }
+}
