e8da26a40f8072717023a3a89837c31508b07b40
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-09 14:45:21 +0100

    fence opens for both players
    Former-commit-id: a37d1a4d704915f6bcd5958d564eba3e436d8757

--- Freight/Assets/Scripts/Photon/BreakFencePhoton.cs
+++ Freight/Assets/Scripts/Photon/BreakFencePhoton.cs
@@ -7,10 +7,11 @@ public class BreakFencePhoton : MonoBehaviourPun
 {
     public GameObject text;
     private GameObject[] players;
+    private bool isBroken;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isBroken = false;
     }
 
     [PunRPC]
@@ -25,13 +26,16 @@ public class BreakFencePhoton : MonoBehaviourPun
         text.SetActive(false);
     }
 
+    [PunRPC]
+    void DestroyFence()
+    {
+        PhotonNetwork.Destroy(transform.gameObject);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (!PhotonNetwork.IsMasterClient)
-        {
-            return;
-        }
+        if (isBroken) return;
         players = GameObject.FindGameObjectsWithTag("Player");
         foreach (var player in players)
         {
@@ -51,28 +55,15 @@ public class BreakFencePhoton : MonoBehaviourPun
             float tempDist = Vector3.Distance(player.transform.position, transform.position);
             string gesture = player.GetComponent<PhotonPlayer>().gesture;
             bool pPressed = player.GetComponent<PhotonPlayer>().IsPressingP();
-            //Debug.Log(gesture);
             if ((gesture.CompareTo("P") == 0 || pPressed) && tempDist <= 2.5f)
             {
                 Vector3 spawnPosition = transform.position;
-                //gameObject.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.MasterClient);
-                PhotonNetwork.Destroy(transform.gameObject);
+
+                photonView.RPC("DestroyFence", RpcTarget.MasterClient);
                 PhotonNetwork.Instantiate("PhotonPrefabs/fence_simple_broken_open Variant 1", spawnPosition, Quaternion.Euler(0f, 90f, 0f));
-                //NetworkServer.Spawn(brokenFence);
+                isBroken = true;
                 break;
             }
         }
-
-        //if (Input.GetKeyDown(KeyCode.P))
-        //{
-        //    float dist = Vector3.Distance(NetworkClient.connection.identity.transform.position, transform.position);
-        //    if (dist <= 2.5f)
-        //    {
-        //        Vector3 spawnPosition = transform.position;
-        //        PhotonNetwork.Destroy(transform.gameObject);
-        //        PhotonNetwork.Instantiate("PhotonPrefabs/fence_simple_broken_open Variant 1", spawnPosition, Quaternion.Euler(0f, 90f, 0f));
-        //        //NetworkServer.Spawn(brokenFence);
-        //    }
-        //}
     }
 }
