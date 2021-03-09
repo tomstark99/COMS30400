8f916e68a69c61f6d17e23c49df806ac2c896130
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-09 17:57:46 +0100

    goes into separate room now :D
    Former-commit-id: 4f5113dcae2161f1fdd9adfc95e2b4d05fdfbeff

---
+++ Freight/Assets/Scripts/Photon/Lobby/CurrentRoomCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentRoomCanvas : MonoBehaviour
+{
+    private RoomsCanvases roomsCanvases;
+
+    public void Initialise(RoomsCanvases canvases)
+    {
+        roomsCanvases = canvases;
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Freight/Assets/Scripts/Photon/Lobby/RoomsCanvases.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomsCanvases : MonoBehaviour
+{
+    [SerializeField]
+    private CreateOrJoinRoomCanvas createOrJoinRoomCanvas;
+    public CreateOrJoinRoomCanvas CreateOrJoinRoomCanvas { get { return createOrJoinRoomCanvas; } }
+
+    [SerializeField]
+    private CurrentRoomCanvas currentRoomCanvas;
+    public CurrentRoomCanvas CurrentRoomCanvas { get { return currentRoomCanvas; } }
+
+    private void Awake()
+    {
+        FirstInitalise();
+    }
+    
+    private void FirstInitalise()
+    {
+        CreateOrJoinRoomCanvas.Initialise(this);
+        CurrentRoomCanvas.Initialise(this);
+    }
+
+}
