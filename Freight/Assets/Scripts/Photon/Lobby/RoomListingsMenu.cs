5b347c86968f5e3ad826efe53a0a3fd8b1740cf3
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-09 17:31:59 +0100

    move room creation files to seperate folder
    Former-commit-id: 3d23b283084af3fa8393462eb181d9ce436e364f

---
+++ Freight/Assets/Scripts/Photon/Lobby/RoomListingsMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomListingsMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Transform content;
+    [SerializeField]
+    private RoomListing roomListing;
+
+    private List<RoomListing> listings = new List<RoomListing>();
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (RoomInfo info in roomList)
+        {
+            if (info.RemovedFromList)
+            {
+                // finds listing to remove and removes it from list of rooms and fromt he UI
+                int index = listings.FindIndex(x => x.RoomInfo.Name == info.Name);
+                if (index != -1)
+                {
+                    Destroy(listings[index].gameObject);
+                    listings.RemoveAt(index);
+                }
+            } 
+            else
+            {
+                // creates listing and adds to list of rooms
+                RoomListing listing = Instantiate(roomListing, content);
+                if (listing != null)
+                {
+                    listing.SetRoomInfo(info);
+                    listings.Add(listing);
+                }
+            }
+            
+        }
+    }
+}
