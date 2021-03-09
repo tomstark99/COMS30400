3d23b283084af3fa8393462eb181d9ce436e364f
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-09 17:31:59 +0100

    move room creation files to seperate folder

--- Freight/Assets/Scripts/Photon/Lobby/RoomListing.cs
+++ Freight/Assets/Scripts/Photon/Lobby/RoomListing.cs
@@ -10,8 +10,11 @@ public class RoomListing : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI text;
 
+    public RoomInfo RoomInfo { get; private set; }
+
     public void SetRoomInfo(RoomInfo roomInfo)
     {
+        RoomInfo = roomInfo;
         text.text = roomInfo.MaxPlayers + ", " + roomInfo.Name;
     }
 
