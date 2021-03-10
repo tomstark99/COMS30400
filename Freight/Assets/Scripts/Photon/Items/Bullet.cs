717a30158408656f39f98b8ff9f0527b97521772
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-10 19:41:56 +0100

    bullet shooting by RPC

---
+++ Freight/Assets/Scripts/Photon/Items/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GetComponent<Rigidbody>().AddForce(GetComponent<PhotonView>(). * 1000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
