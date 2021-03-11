9ca54430b8c7f7b82ca09f38ee0cd9f702127f9f
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-11 21:10:34 +0100

    bullet selfdestructs

--- Freight/Assets/Scripts/Photon/Items/Bullet.cs
+++ Freight/Assets/Scripts/Photon/Items/Bullet.cs
@@ -8,12 +8,13 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //GetComponent<Rigidbody>().AddForce(GetComponent<PhotonView>(). * 1000);
+        StartCoroutine(SelfDestruct());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SelfDestruct()
     {
-        
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
     }
+
 }
