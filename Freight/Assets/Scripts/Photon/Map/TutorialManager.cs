68994e908b4c9a06afe317b5d2bf563885702166
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-15 18:06:49 +0100

    changed tutorial manager so its attatched to tutorial player prefab

---
+++ Freight/Assets/Scripts/Photon/Map/TutorialManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TutorialManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private GameObject player;
+
+    private int tutorialCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tutorialCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tutorialCounter == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                tutorialCounter++;
+
+            }
+        }
+    }
+
+}
