9be9d7ae58fc1f88fed05ab3462ea902a7dd55b3
Stefan Pryuna <mc18112@bristol.ac.uk>
2021-03-15 17:05:44 +0200

    Added mouse, moves a bit

---
+++ Freight/Assets/Scripts/Photon/TutorialMap/MouseMovesScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovesScript : MonoBehaviour
+{
+    Vector3 aPos;
+    float movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementSpeed = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z);
+
+    }
+}
