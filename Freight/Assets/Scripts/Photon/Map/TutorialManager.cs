6483c2b584d6c3145ff9b2cd7982607d62f9e6ea
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-15 19:18:19 +0100

    added WASD keys, disappear after you press them all

--- Freight/Assets/Scripts/Photon/Map/TutorialManager.cs
+++ Freight/Assets/Scripts/Photon/Map/TutorialManager.cs
@@ -5,15 +5,34 @@ using Photon.Pun;
 
 public class TutorialManager : MonoBehaviourPunCallbacks
 {
+    [Header("WASD")]
+    [SerializeField]
+    private GameObject wSprite;
+    [SerializeField]
+    private GameObject aSprite;
+    [SerializeField]
+    private GameObject sSprite;
+    [SerializeField]
+    private GameObject dSprite;
+    [SerializeField]
+    private GameObject playerMovement;
+
+    [Header("Player")]
     [SerializeField]
     private GameObject player;
 
     private int tutorialCounter;
+    private bool wPressed;
+    private bool aPressed;
+    private bool sPressed;
+    private bool dPressed;
+    private int keysPressed;
 
     // Start is called before the first frame update
     void Start()
     {
         tutorialCounter = 0;
+        keysPressed = 0;
     }
 
     // Update is called once per frame
@@ -21,10 +40,35 @@ public class TutorialManager : MonoBehaviourPunCallbacks
     {
         if (tutorialCounter == 0)
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.W) && !wPressed)
             {
-                tutorialCounter++;
+                wPressed = true;
+                keysPressed++;
+                wSprite.SetActive(true);
+            }
+            if (Input.GetKeyDown(KeyCode.A) && !aPressed)
+            {
+                aPressed = true;
+                keysPressed++;
+                aSprite.SetActive(true);
+            }
+            if (Input.GetKeyDown(KeyCode.S) && !sPressed)
+            {
+                sPressed = true;
+                keysPressed++;
+                sSprite.SetActive(true);
+            }
+            if (Input.GetKeyDown(KeyCode.D) && !dPressed)
+            {
+                dPressed = true;
+                keysPressed++;
+                dSprite.SetActive(true);
+            }
+            if (keysPressed == 4)
+            {
 
+                tutorialCounter++;
+                Destroy(playerMovement);
             }
         }
     }
