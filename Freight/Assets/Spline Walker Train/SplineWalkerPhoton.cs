9b4dcda366640278fd4c79156f1a1c3fb00d59be
tomstark99 <jb18789@bristol.ac.uk>
2021-03-24 17:39:15 +0000

    Terran 2.0
    - Fixed terrain again to fit new layout
    - Changed time to leave for train to 60 seconds for testing better

--- Freight/Assets/Spline Walker Train/SplineWalkerPhoton.cs
+++ Freight/Assets/Spline Walker Train/SplineWalkerPhoton.cs
@@ -16,7 +16,7 @@ public class SplineWalkerPhoton : MonoBehaviourPun
     private float progress;
     private bool goingForward = true;
     // private bool leaveStation = false;
-    private float timeToLeave = 5.0f;
+    private float timeToLeave = 60.0f;
 
     public Vector3 trainPos;
     public Vector3 prevPos;
