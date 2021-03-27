b94981418166291447b986619e7464a256d75136
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-27 18:16:18 +0100

    loading screen started, works for 1 player so far
    don't know how well it will work since webGL is single threaded
    
    Former-commit-id: a55d55bd81d72c3969d0d330f1dd2efca3771782

---
+++ Freight/Assets/Scripts/Photon/Lobby/LoadingManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoadingManager : MonoBehaviourPun
+{
+    public GameObject loadingPanel;
+    public Slider loadingBar;
+    public TextMeshProUGUI loadingText;
+
+    private bool mapLoaded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            photonView.RPC(nameof(LoadLevel), RpcTarget.All);
+        }
+
+        mapLoaded = false;
+    }
+
+    [PunRPC]
+    public void LoadLevel()
+    {
+        StartCoroutine(LoadSceneAsync("Assets/Scenes/TrainStationPun.unity"));
+    }
+
+    IEnumerator LoadSceneAsync(string levelName)
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(levelName);
+        op.allowSceneActivation = false;
+
+        while (!op.isDone)
+        {
+            float progress = Mathf.Clamp01(op.progress / .9f);
+            Debug.Log(op.progress);
+
+            loadingBar.value = op.progress;
+            loadingText.text = 100f * op.progress + "%";
+
+            if (op.progress > 0.9f && mapLoaded == false)
+            {
+                mapLoaded = true;
+            }
+
+            yield return null;
+        }
+    }
+}
