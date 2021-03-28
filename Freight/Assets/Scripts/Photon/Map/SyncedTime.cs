32682a91eb29eed1ae9363ccee6d00ce37f95e12
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-28 15:40:37 +0200

    changed synced time to be on start
    Former-commit-id: 9f0f4f0ec0a389bc13fe63f6efbc64a72ff53f91

--- Freight/Assets/Scripts/Photon/Map/SyncedTime.cs
+++ Freight/Assets/Scripts/Photon/Map/SyncedTime.cs
@@ -12,9 +12,9 @@ public class SyncedTime : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        timeToLeave = 60f;
+        timeToLeave = 10f;
     }
 
     // Update is called once per frame
