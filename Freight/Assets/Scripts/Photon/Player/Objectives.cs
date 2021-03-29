7558d705306bf98dfe5d4790b429a291b5a89a39
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-29 15:16:13 +0200

    event on fence to trigger outline when in rangeTODO: sub event to player
    Former-commit-id: 60cb8a0939a54ce8178192ffdbdf15061ac97c74

---
+++ Freight/Assets/Scripts/Photon/Player/Objectives.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectives : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject findBrokenFence;
+    [SerializeField]
+    private GameObject breakFence;
+    [SerializeField]
+    private GameObject findBackpacks;
+    [SerializeField]
+    private GameObject findTrain;
+    [SerializeField]
+    private GameObject escapeOnTrain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
