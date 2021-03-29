a67cf5621c283e5fae2c3d398d4e35ad2056faae
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-29 19:03:21 +0200

    created new class for backpacks and can pick them up on your back
    Former-commit-id: 7e043d98e92c1ed0950c1c8829e1aced6b704a2d

---
+++ Freight/Assets/Scripts/Photon/Items/Grabbable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Grabbable : PickUpable
+{
+    public override void PrimaryInteraction(Character character)
+    {
+        if (!isPickedUp)
+        {
+            character.Grab(this);
+        }
+    }
+
+    public override void PrimaryInteractionOff(Character character)
+    {
+
+    }
+}
