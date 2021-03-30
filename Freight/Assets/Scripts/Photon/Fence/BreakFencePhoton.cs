3a01b490f6b31d4a7dd1066f75e990622f26e1f9
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-30 12:56:22 +0200

    made fence break an rpc ting
    Former-commit-id: a2470351e021be8e2c27d3bf012e2e1a46418869

--- Freight/Assets/Scripts/Photon/Fence/BreakFencePhoton.cs
+++ Freight/Assets/Scripts/Photon/Fence/BreakFencePhoton.cs
@@ -56,6 +56,8 @@ public class BreakFencePhoton : MonoBehaviourPun
     [PunRPC]
     void DestroyFence()
     {
+        // event
+        FenceBroke();
         PhotonNetwork.Destroy(transform.gameObject);
     }
 
@@ -83,8 +85,6 @@ public class BreakFencePhoton : MonoBehaviourPun
 
                     photonView.RPC("DestroyFence", RpcTarget.MasterClient);
                     PhotonNetwork.Instantiate("PhotonPrefabs/fence_simple_broken_open Variant 1", spawnPosition, Quaternion.Euler(0f, 90f, 0f));
-                    // event
-                    FenceBroke();
                     isBroken = true;
                     break;
                 }
