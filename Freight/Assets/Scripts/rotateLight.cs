4556d6af31dd0c681bb203d168994673b58daeff
Calum West <fq18174@bristol.ac.uk>
2021-03-31 12:35:36 +0100

    rotateLight: Changed variables so they aren't hard-coded
    - Also added another spotlight on top of guard tower
    
    Former-commit-id: 384f6fd0afdc4d39e17385009795b8900ad9e036

--- Freight/Assets/Scripts/rotateLight.cs
+++ Freight/Assets/Scripts/rotateLight.cs
@@ -7,28 +7,38 @@ public class rotateLight : MonoBehaviour
     [SerializeField]
     private GameObject spotlight;
     private bool positiveRotation;
+    private float rotationMultiplier = 0.25f;
+    public bool spinningLeft;
+    public float rotationUpperLimit;
+    public float rotationLowerLimit;
 
     // Start is called before the first frame update
     void Start()
     {
         positiveRotation = true;
+        if (spinningLeft == false)
+        {
+            rotationMultiplier *= -1;
+            positiveRotation = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(spotlight.transform.rotation.y);
         if (positiveRotation == true)
         {
-            spotlight.transform.Rotate(Vector3.up * 0.25f);
-            if (spotlight.transform.rotation.y > 0.35f)
+            spotlight.transform.Rotate(Vector3.up * rotationMultiplier);
+            if (spotlight.transform.rotation.y > rotationUpperLimit)
             {
                 positiveRotation = false;
             }
         }
         else
         {
-            spotlight.transform.Rotate(Vector3.up * -0.25f);
-            if (spotlight.transform.rotation.y < -0.7f)
+            spotlight.transform.Rotate(Vector3.up * -rotationMultiplier);
+            if (spotlight.transform.rotation.y < rotationLowerLimit)
             {
                 positiveRotation = true;
             }
