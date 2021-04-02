4c9a99a1725fb44bab3d582d422095bb5c7e9511
Omar Suleiman <jb18940@bristol.ac.uk>
2021-04-02 14:32:25 +0200

    made player prefab actually able to transfer ownership
    Former-commit-id: 341bd1faedfd0f4fa489f2ef4cad2791ca52acbc

--- Freight/Assets/Scripts/Photon/Lobby/GameSetupController.cs
+++ Freight/Assets/Scripts/Photon/Lobby/GameSetupController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using System.IO;
-public class GameSetupController : MonoBehaviour
+public class GameSetupController : MonoBehaviourPunCallbacks, IPunOwnershipCallbacks
 {
     /*
      * All this script is doing in the end is creating the player object. The game is told to find this player object under a 
@@ -18,13 +18,28 @@ public class GameSetupController : MonoBehaviour
     private void CreatePlayer()
     {
         Debug.Log("Creating Player");
+        int i = 0;
         foreach (var p in PhotonNetwork.PlayerList)
         {
-            GameObject player = PhotonNetwork.Instantiate("PhotonPrefabs/PhotonPlayer", new Vector3(222, 4, 294), Quaternion.identity);
+            Debug.Log(p);
+            GameObject player = PhotonNetwork.Instantiate("PhotonPrefabs/PhotonPlayer", new Vector3(222, 4, 294 + i), Quaternion.identity);
             if (p != PhotonNetwork.LocalPlayer)
-            player.GetComponent<PhotonView>().TransferOwnership(p);
+                player.GetComponent<PhotonView>().TransferOwnership(p);
+
+            i += 3;
         }
         
         
+    }
+
+    public void OnOwnershipRequest(PhotonView targetView, Photon.Realtime.Player previousOwner)
+    {
+
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Photon.Realtime.Player previousOwner)
+    {
+        targetView.OwnershipTransfer = OwnershipOption.Fixed;
+        Debug.Log("UNITY IS IARRIRIRR");
     }
 }
