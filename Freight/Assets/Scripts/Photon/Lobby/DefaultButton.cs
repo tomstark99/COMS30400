755e06c0c34941c653195fa4fef1311e1ff75652
Omar Suleiman <jb18940@bristol.ac.uk>
2021-04-12 17:43:16 +0200

    added default button, need to add slider change when pressed
    Former-commit-id: 214656ccffb16648d16c700ce55296387ad4ccbe

---
+++ Freight/Assets/Scripts/Photon/Lobby/DefaultButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class DefaultButton : MonoBehaviour
+{
+    void Start()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            gameObject.transform.GetChild(0).GetComponent<Button>().interactable = false;
+        }
+    }
+
+    public void SetSettingsToDefault()
+    {
+        ExitGames.Client.Photon.Hashtable props = new ExitGames.Client.Photon.Hashtable();
+
+        props.Add("sliderValue", 200);
+        props.Add("sliderValueDiff", "Hard");
+
+        PhotonNetwork.CurrentRoom.SetCustomProperties(props);
+    }
+}
