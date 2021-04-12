a3e4944690c51e80089a4746236119cf71191f6d
Omar Suleiman <jb18940@bristol.ac.uk>
2021-04-12 13:59:06 +0200

    Added settings to menu
    Former-commit-id: 266d0109bb4eb06bacf0309e657e18c9c41c8134

---
+++ Freight/Assets/Scripts/Photon/Map/GameSettings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    private int guardSightRange;
+    private int guardAngle;
+    private int speedChasing;
+    private int speedPatrolling;
+    private int timeToLeave;
+    private float spotlightRotationSpeed;
+    private int ammoAmount;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Freight/Assets/Scripts/Photon/UIUtils/SliderToValue.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+// This script can be applied to sliders to get the value of the slider to show up as text on the UI
+public class SliderToValue : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+    [SerializeField]
+    private TextMeshProUGUI text;
+
+    [SerializeField]
+    private bool difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!difficulty)
+            UpdateSliderValue();
+        else
+            UpdateSliderValueDifficulty();
+    }
+
+    public void UpdateSliderValue()
+    {
+        text.text = slider.value.ToString();
+    }
+
+    // special case function for easy/med/hard/impossible 
+    public void UpdateSliderValueDifficulty()
+    {
+        int sliderValue = (int) slider.value;
+
+        if (sliderValue == 0)
+            text.text = "Easy";
+        else if (sliderValue == 1)
+            text.text = "Medium";
+        else if (sliderValue == 2)
+            text.text = "Hard";
+        else if (sliderValue == 3)
+            text.text = "Impossible";
+    }
+}
