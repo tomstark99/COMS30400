26e59b4febded91095c2013e89fd85e2ca6e0ed8
Omar Suleiman <jb18940@bristol.ac.uk>
2021-04-12 15:22:15 +0200

    time is set from settings and spotlights are deactivated

---
+++ Freight/Assets/Scripts/Photon/Map/Spotlights.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spotlights : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!GameObject.Find("GameSettings").GetComponent<GameSettings>().SpotlightsActive)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
