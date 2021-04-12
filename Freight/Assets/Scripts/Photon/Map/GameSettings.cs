b50643719b5f2b8fc88e300ab276c7b1525990d7
Omar Suleiman <jb18940@bristol.ac.uk>
2021-04-12 15:11:11 +0200

    Saving the values on game start, TODO: need to apply to objects ingame

--- Freight/Assets/Scripts/Photon/Map/GameSettings.cs
+++ Freight/Assets/Scripts/Photon/Map/GameSettings.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSettings : MonoBehaviour
 {
@@ -9,23 +10,71 @@ public class GameSettings : MonoBehaviour
     private int speedChasing;
     private int speedPatrolling;
     private int timeToLeave;
-    private float spotlightRotationSpeed;
-    private int ammoAmount;
+    private bool spotlightsRotating;
+    private bool spotlightsActive;
 
     void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void SetGameSettings()
     {
-        
-    }
+        GameObject guardDifficulty = GameObject.Find("Canvases/OverlayCanvases/CurrentRoomCanvas/GameSettingsObject/GuardDifficulty/Slider");
+        int guardDifficultyVal = (int)guardDifficulty.GetComponent<Slider>().value;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // easy
+        if (guardDifficultyVal == 0)
+        {
+            guardSightRange = 10;
+            guardAngle = 50;
+            speedChasing = 4;
+            speedPatrolling = 2;
+            spotlightsActive = false;
+            spotlightsRotating = false;
+        } 
+        // medium
+        else if (guardDifficultyVal == 1)
+        {
+            guardSightRange = 20;
+            guardAngle = 65;
+            speedChasing = 6;
+            speedPatrolling = 3;
+            spotlightsActive = true;
+            spotlightsRotating = false;
+        }
+        // hard
+        else if (guardDifficultyVal == 2)
+        {
+            guardSightRange = 30;
+            guardAngle = 80;
+            speedChasing = 7;
+            speedPatrolling = 4;
+            spotlightsActive = true;
+            spotlightsRotating = true;
+        }
+        // impossible
+        else if (guardDifficultyVal == 3)
+        {
+            guardSightRange = 40;
+            guardAngle = 100;
+            speedChasing = 10;
+            speedPatrolling = 6;
+            spotlightsActive = true;
+            spotlightsRotating = true;
+        }
+
+        GameObject timeToLeaveObject = GameObject.Find("Canvases/OverlayCanvases/CurrentRoomCanvas/GameSettingsObject/TimeToLeave/Slider");
+        timeToLeave =  (int)timeToLeaveObject.GetComponent<Slider>().value;
+
+        Debug.Log("Sight range: " + guardSightRange);
+        Debug.Log("Angle: " + guardAngle);
+        Debug.Log("Chasing Speed: " + speedChasing);
+        Debug.Log("Patrolling Speed: " + speedPatrolling);
+        Debug.Log("Spotlights active? " + spotlightsActive);
+        Debug.Log("Spotlights rotating? " + spotlightsRotating);
+        Debug.Log("Time to leave " + timeToLeave);
+
+
     }
 }
