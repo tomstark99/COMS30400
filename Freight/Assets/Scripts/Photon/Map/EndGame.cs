f0dd7a59d2649f826cc4d9d7bb88d283302dfa8d
Calum West <fq18174@bristol.ac.uk>
2021-04-12 11:02:35 +0100

    UI removed when game won + ended

--- Freight/Assets/Scripts/Photon/Map/EndGame.cs
+++ Freight/Assets/Scripts/Photon/Map/EndGame.cs
@@ -113,7 +113,7 @@ public class EndGame : MonoBehaviour
                     
                 }
 
-                //EndTheGame();
+                EndTheGame();
                 
             }
         }
