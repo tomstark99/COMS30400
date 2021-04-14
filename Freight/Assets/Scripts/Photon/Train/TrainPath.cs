91b1e4206e38003d28fc42d022dc8e6684572e67
tomstark99 <jb18789@bristol.ac.uk>
2021-04-14 18:43:52 +0100

    iTween migration
    - fix ur spline walkerrrr accc
    
    Former-commit-id: a48fcfbdcd8b21d9aa6ef2666579b985ba4e874b

---
+++ Freight/Assets/Scripts/Photon/Train/TrainPath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TrainPath : MonoBehaviourPun
+{
+    public GameObject train;
+    public Transform[] points;
+
+    [Range(0,1)]
+    public float position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iTween.MoveTo(train, iTween.Hash("path", points, "speed", 5.0f, "orientToPath", true, "easetype", iTween.EaseType.easeInOutSine, "axis", "y", "lookahead", 0.001f));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (position < 1) {
+            position += Time.deltaTime / 10;
+        }
+        // iTween.PutOnPath(train, points, position);
+
+    }
+
+    private void OnDrawGizmos() {
+        iTween.DrawPath(points);
+    }
+}
