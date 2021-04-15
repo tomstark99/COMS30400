e578a439406cf3c71c5741a5821948962e1245a7
tomstark99 <jb18789@bristol.ac.uk>
2021-04-15 11:58:17 +0100

    added callback at end of animation to prevent lag
    - at end of animation, stops placing carriages on the track

--- Freight/Assets/Scripts/Photon/Train/TrainPath.cs
+++ Freight/Assets/Scripts/Photon/Train/TrainPath.cs
@@ -32,19 +32,20 @@ public class TrainPath : MonoBehaviourPun
             if (timeToLeave < 0 && !left) StartAnimation();
             if (left) {
                 foreach (var c in carriages) {
-                    float min = float.PositiveInfinity;
-                    float min_p = 0.0f;
-                    for (float t = 0; t <= 1; t += 0.0005f) {
-                        float dist = (c.transform.position - iTween.PointOnPath(points, t)).sqrMagnitude;
-                        if (dist < min) {
-                            min = dist;
-                            min_p = t;
-                        }
-                    }
-                    if (min < 0.001f && !carriageOnTrack[c]) {
+                    // Debug.Log(((c.transform.position - iTween.PointOnPath(points, 0.0f)).sqrMagnitude < 0.01f));
+                    if ((c.transform.position - iTween.PointOnPath(points, 0.0f)).sqrMagnitude < 0.1f && !carriageOnTrack[c]) {
                         Debug.Log("ONTRACK");
                         carriageOnTrack[c] = true;
                     } else if (carriageOnTrack[c]) {
+                        float min = float.PositiveInfinity;
+                        float min_p = 0.0f;
+                        for (float t = 0; t <= 1; t += 0.0005f) {
+                            float dist = (c.transform.position - iTween.PointOnPath(points, t)).sqrMagnitude;
+                            if (dist < min) {
+                                min = dist;
+                                min_p = t;
+                            }
+                        }
                         iTween.PutOnPath(c, points, min_p);
                     }
                 }
@@ -62,7 +63,20 @@ public class TrainPath : MonoBehaviourPun
             - iTween.EaseType.linear
         */
         Debug.Log("mans animating");
-        iTween.MoveTo(train, iTween.Hash("path", points, "speed", 7.0f, "orientToPath", true, "easetype", iTween.EaseType.linear, "lookahead", 0.005f));
         left = true;
+        iTween.MoveTo(train, iTween.Hash("path", points, 
+                                        "speed", 7.0f, 
+                                        "orientToPath", true, 
+                                        "easetype", iTween.EaseType.linear, 
+                                        "lookahead", 0.005f,
+                                        "oncompletetarget", gameObject,  
+                                        "oncomplete", "onAnimComplete"));
+
+    }
+
+    public void onAnimComplete() {
+        foreach (var c in carriages) {
+            carriageOnTrack[c] = false;
+        }
     }
 }
