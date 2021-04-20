2e7dc45425c322a0a02bd864c097152131bef47b
Omar Suleiman <jb18940@bristol.ac.uk>
2021-04-20 13:25:52 +0200

    added intro audio clip and subtitles
    Former-commit-id: f6dcb1e860638648823d4f1798dbb7391331de90

---
+++ Freight/Assets/Scripts/Photon/Player/PlayerAudioClips.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerAudioClips : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI subtitle;
+    [SerializeField]
+    private GameObject intro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(IntroSequence());
+    }
+
+    IEnumerator IntroSequence()
+    {
+        yield return new WaitForSeconds(1f);
+        subtitle.text = "Bossman: Mission's simple, get in, get the bags, get out, any mean's necessary";
+        intro.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        subtitle.text = "";
+    }
+}
