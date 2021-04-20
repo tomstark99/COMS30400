bacb7fa668a8544577d81e39ec30a46d32f5cc2f
Stefan Pryuna <mc18112@bristol.ac.uk>
2021-04-20 18:18:25 +0300

    Added text and scripts for the first achievement
    Former-commit-id: 906ee887a61555e22c13fad662e628e433a2b9d7

---
+++ Freight/Assets/Scripts/AchievementBadge.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementBadge : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void  OnMouseOver() {
+       
+       gameObject.GetComponent<RectTransform>().localScale = new Vector2(1.15f, 1.15f);
+    }
+
+    public void  OnMouseExit() {
+       gameObject.GetComponent<RectTransform>().localScale = new Vector2(1.0f, 1.0f);
+    }
+}
