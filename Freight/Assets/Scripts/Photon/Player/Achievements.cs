7bfa680129d2556c0fe8431b955ed45f4d0fc480
Omar Suleiman <jb18940@bristol.ac.uk>
2021-04-22 14:26:43 +0200

    added scene object achievement

--- Freight/Assets/Scripts/Photon/Player/Achievements.cs
+++ Freight/Assets/Scripts/Photon/Player/Achievements.cs
@@ -15,6 +15,7 @@ public class Achievements : MonoBehaviourPun
     private GameObject letTheHuntBegin;
     private GameObject useNature;
     private GameObject roadman;
+    private GameObject theCompletePicture;
 
     private Queue<IEnumerator> coroutineQueue = new Queue<IEnumerator>();
 
@@ -42,10 +43,15 @@ public class Achievements : MonoBehaviourPun
             {
                 achievementsTab.transform.GetChild(0).GetChild(4).GetComponent<Image>().color = tempColor;
             }
+            if (PlayerPrefs.HasKey("TheCompletePicture"))
+            {
+                achievementsTab.transform.GetChild(0).GetChild(5).GetComponent<Image>().color = tempColor;
+            }
             babySteps = achievements.transform.GetChild(0).gameObject;
             letTheHuntBegin = achievements.transform.GetChild(1).gameObject;
             useNature = achievements.transform.GetChild(3).gameObject;
             roadman = achievements.transform.GetChild(4).gameObject;
+            theCompletePicture = achievements.transform.GetChild(5).gameObject;
 
             StartCoroutine(CoroutineCoordinator());
         }
@@ -157,5 +163,29 @@ public class Achievements : MonoBehaviourPun
         roadman.SetActive(true);
         yield return new WaitForSeconds(3f);
         roadman.SetActive(false);
+    }
+
+    public void TheCompletePictureCompleted()
+    {
+        if (photonView.IsMine)
+        {
+            if (!PlayerPrefs.HasKey("TheCompletePicture"))
+            {
+                PlayerPrefs.SetInt("TheCompletePicture", 1);
+                PlayerPrefs.Save();
+                var tempColor = achievementsTab.transform.GetChild(0).GetChild(5).GetComponent<Image>().color;
+                tempColor.a = 1f;
+                achievementsTab.transform.GetChild(0).GetChild(5).GetComponent<Image>().color = tempColor;
+                //StartCoroutine(RoadmanSequence());
+                coroutineQueue.Enqueue(TheCompletePictureSequence());
+            }
+        }
+    }
+
+    IEnumerator TheCompletePictureSequence()
+    {
+        theCompletePicture.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        theCompletePicture.SetActive(false);
     }
 }
