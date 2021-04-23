962f510debb3e4c216390e847f0304119c050cde
Vali <bk18529@bristol.ac.uk>
2021-04-23 14:14:51 +0300

    added new pose for moving fwd, anim still doesn't work :(
    Former-commit-id: 3f8d8f43309be1fb760bd24c3c3531b4de993527

--- html/sketch.js
+++ html/sketch.js
@@ -71,12 +71,13 @@ function modelLoaded() {
 function noseLabel(){
     // normalise nose position e.g. 0<x,y<1
     var normNosePos = createVector(pose1.nose.x/(2 * width), pose1.nose.y/(2 * height));
-    // if(normNosePos.x > 0 && normNosePos.x < 0.37 && normNosePos.y > 1/3 && normNosePos.y < 2/3){
-    //   return 'I';
-    // }else if (normNosePos.x > 0.62 && normNosePos.x < 1 && normNosePos.y > 1/3 && normNosePos.y < 2/3) {
-    //   return 'O';
-    // }else
-    if (normNosePos.x > 0 && normNosePos.x < 1/3 && normNosePos.y > 2/3 && normNosePos.y < 1) {
+    if(normNosePos.x >1/3 && normNosePos.x < 2/3 && normNosePos.y > 0.2 && normNosePos.y < 0.4){
+        return 'F';
+    } else if(normNosePos.x > 0 && normNosePos.x < 0.37 && normNosePos.y > 1/3 && normNosePos.y < 2/3){
+        return 'I';
+    }else if (normNosePos.x > 0.62 && normNosePos.x < 1 && normNosePos.y > 1/3 && normNosePos.y < 2/3) {
+        return 'O';
+    }else if (normNosePos.x > 0 && normNosePos.x < 1/3 && normNosePos.y > 2/3 && normNosePos.y < 1) {
         return 'W';
     } else if (normNosePos.x > 2/3 && normNosePos.x < 1 && normNosePos.y > 2/3 && normNosePos.y < 1) {
         return 'Q';
@@ -88,39 +89,39 @@ function noseLabel(){
 }
 
 function handsLabel(){
-  if (pose1.leftWrist.x < width * 2 && pose1.leftWrist.x > 0 && pose1.leftWrist.y < height * 2 && pose1.leftWrist.y > 0) {
-    //if(pose3.leftWrist.confidence>0.7 && pose3.rightWrist.confidence>0.7){
-    // normalise wrist positions e.g. 0<x,y<1
-    var normLeftWristVector = createVector((pose3.leftWrist.x-pose1.leftWrist.x)/(2 * width), (pose3.leftWrist.y-pose1.leftWrist.y)/(2 * height));
-    var normRightWristVector = createVector((pose3.rightWrist.x-pose1.rightWrist.x)/(2 * width), (pose3.rightWrist.y-pose1.rightWrist.y)/(2 * width));
-    var normLeftWristPos = createVector(pose1.leftWrist.x/(2 * width), pose1.leftWrist.y/(2 * height));
-    if (normLeftWristVector.x<-0.1) {
-          // Pick up, both hands moving up
-          poseLag = 3;
-          return "B";
-    } else if ((normLeftWristVector.x>0.07 && normRightWristVector.x<-0.07)) {
-        // Pull apart, both hands moving apart
-        poseLag = 4;
-        return 'P';
-    }else if (normLeftWristVector.y>0.07 && normRightWristVector.y>0.07) {
-        // Pull up, both hands moving down
-        poseLag = 2;
-        return "U";
+    if (pose1.leftWrist.x < width * 2 && pose1.leftWrist.x > 0 && pose1.leftWrist.y < height * 2 && pose1.leftWrist.y > 0) {
+        //if(pose3.leftWrist.confidence>0.7 && pose3.rightWrist.confidence>0.7){
+        // normalise wrist positions e.g. 0<x,y<1
+        var normLeftWristVector = createVector((pose3.leftWrist.x-pose1.leftWrist.x)/(2 * width), (pose3.leftWrist.y-pose1.leftWrist.y)/(2 * height));
+        var normRightWristVector = createVector((pose3.rightWrist.x-pose1.rightWrist.x)/(2 * width), (pose3.rightWrist.y-pose1.rightWrist.y)/(2 * width));
+        var normLeftWristPos = createVector(pose1.leftWrist.x/(2 * width), pose1.leftWrist.y/(2 * height));
+        if (normLeftWristVector.x<-0.1) {
+            // Pick up, both hands moving up
+            poseLag = 3;
+            return "B";
+        } else if ((normLeftWristVector.x>0.07 && normRightWristVector.x<-0.07)) {
+            // Pull apart, both hands moving apart
+            poseLag = 4;
+            return 'P';
+        }else if (normLeftWristVector.y>0.07 && normRightWristVector.y>0.07) {
+            // Pull up, both hands moving down
+            poseLag = 2;
+            return "U";
 
         // }else if (normLeftWristPos.x>2/3 && normLeftWristPos.x<1 && normLeftWristPos.y>0.2 && normLeftWristPos.y<0.8) {
         //     // Move forward, left hand up
         //     return "F";
-    } else if (normLeftWristVector.y>0.4) {
-        return "R";
-      }else if((normLeftWristVector.y>0.1 && normRightWristVector.y<-0.1) || (normLeftWristVector.y<-0.1 && normRightWristVector.y>0.1)){
+        } else if (normLeftWristVector.y>0.4) {
+            return "R";
+        }else if((normLeftWristVector.y>0.1 && normRightWristVector.y<-0.1) || (normLeftWristVector.y<-0.1 && normRightWristVector.y>0.1)){
             // Ladder climb, hands moving in opposite directions
             poseLag = 12;
             return 'L';
-    }else{
-        return 'N';
+        }else{
+            return 'N';
+        }
+        //}
     }
-    //}
-  }
     return 'N';
 }
 
@@ -296,14 +297,6 @@ function draw() {
     // |N|N|N|
     // |O|N|I|
     // |Q|C|W|
-    //
-    // if (point.x > 1/3 && point.x < 2/3 && y > 2/3 && y < 1) {
-    //   label = C;
-    // }
-    // ...
-    // else {
-    //   label = N;
-    // }
 
     // fill(255, 0, 255);
     // noStroke();
