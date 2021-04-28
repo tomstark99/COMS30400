ae3e4528d16be813b8372dc14d9b30ac861561f8
Omar Suleiman <jb18940@bristol.ac.uk>
2021-04-28 17:17:12 +0200

    got bag drop off objective
    Former-commit-id: 11157d0fd3fb7accf1d8d8613b722899394ab7d9

--- Freight/Assets/Scripts/Photon/Player/ObjectivesSecond.cs
+++ Freight/Assets/Scripts/Photon/Player/ObjectivesSecond.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
+using System;
 
 public class ObjectivesSecond : MonoBehaviour
 {
@@ -18,18 +20,70 @@ public class ObjectivesSecond : MonoBehaviour
 
     private bool twoPlayers;
 
+    private GameObject drop1;
+    private GameObject drop2;
+
+    private int counter;
+
     // Start is called before the first frame update
     void Start()
     {
+        drop1 = GameObject.FindGameObjectWithTag("DropSpot");
+        drop1.GetComponent<Droppable>().BagDropped += DropBags;
+        counter = 1;
+
         if (PhotonNetwork.CurrentRoom.PlayerCount > 1)
         {
             twoPlayers = true;
+            dropBagsDistance2.SetActive(true);
+            drop2 = GameObject.FindGameObjectWithTag("DropSpot2");
+            drop2.GetComponent<Droppable>().BagDropped += DropBags;
+            counter = 2;
+        }
+
+    }
+
+    void DropBags()
+    {
+        counter -= 1;
+
+        if (counter == 0)
+        {
+            dropBagsDistance.SetActive(false);
+            dropBagsDistance2.SetActive(false);
+            dropBagsDesc.SetActive(false);
+        }
+        else
+        {
+            if (drop1.GetComponent<Droppable>().isDroppedOff)
+            {
+                dropBagsDistance.GetComponent<TextMeshProUGUI>().fontStyle = FontStyles.Strikethrough;
+            } 
+            else
+            {
+                dropBagsDistance2.GetComponent<TextMeshProUGUI>().fontStyle = FontStyles.Strikethrough;
+            }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (dropBagsDesc.activeSelf)
+        {
+            if (!drop1.GetComponent<Droppable>().isDroppedOff)
+            {
+                float distance = Vector3.Distance(transform.position, drop1.transform.position);
+
+                dropBagsDistance.GetComponent<TextMeshProUGUI>().text = "-Drop point 1 distance: " + Math.Round(distance, 2) + "m";
+            }
+            if (twoPlayers)
+                if (!drop2.GetComponent<Droppable>().isDroppedOff)
+                {
+                    float distance = Vector3.Distance(transform.position, drop2.transform.position);
+
+                    dropBagsDistance2.GetComponent<TextMeshProUGUI>().text = "-Drop point 2 distance: " + Math.Round(distance, 2) + "m";
+                }
+        }
     }
 }
