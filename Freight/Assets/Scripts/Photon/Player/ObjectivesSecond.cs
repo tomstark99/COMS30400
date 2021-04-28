cd4598995fd0d8d5839ed893d9cc9920af2067e1
Omar Suleiman <jb18940@bristol.ac.uk>
2021-04-28 14:34:47 +0200

    starting objectives for second level
    Former-commit-id: 0c9e50361f066dd426693541f87a057b61c8df45

--- Freight/Assets/Scripts/Photon/Player/ObjectivesSecond.cs
+++ Freight/Assets/Scripts/Photon/Player/ObjectivesSecond.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class ObjectivesSecond : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject dropBags;
+    [SerializeField]
+    private GameObject dropBagsDistance;
+    [SerializeField]
+    private GameObject dropBagsDistance2;
+    [SerializeField]
+    private GameObject dropBagsDesc;
+    [SerializeField]
+    private GameObject dropBagsBackground;
+
+    private bool twoPlayers;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PhotonNetwork.CurrentRoom.PlayerCount > 1)
+        {
+            twoPlayers = true;
+        }
     }
 
     // Update is called once per frame
