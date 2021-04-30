f621572a2be2fc2c4d38db28df643b40f1fb2e54
Omar Suleiman <jb18940@bristol.ac.uk>
2021-04-30 13:55:28 +0200

    got basic endgame script to change camera, need to implement multiplayer

--- Freight/Assets/Scripts/Photon/Map/EndGameSecond.cs
+++ Freight/Assets/Scripts/Photon/Map/EndGameSecond.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Photon.Pun;
+using Cinemachine;
 
-public class EndGameSecond : MonoBehaviour
+public class EndGameSecond : MonoBehaviourPun
 {
+    public event Action PlayerReadyToLeave;
+
+    [SerializeField]
+    private GameObject endGameCamera;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +27,37 @@ public class EndGameSecond : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.tag);
+        if (PlayerReadyToLeave != null)
+            PlayerReadyToLeave();
+    }
 
+    public void EndTheGame()
+    {
+        endGameCamera.GetComponent<CinemachineVirtualCamera>().Priority = 101;
+    }
+
+    [PunRPC]
+    void CheckEndGameRPC()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        int counter = 0;
+        foreach (var player in players)
+        {
+            if (player.GetComponent<ObjectivesSecond>().readyToLeave)
+                counter++;
+        }
+
+        if (counter == 2)
+        {
+            EndTheGame();
+        } 
+    }
+
+    public void CheckEndGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            photonView.RPC(nameof(CheckEndGameRPC), RpcTarget.MasterClient);
+        }
     }
 }
