51614649d38d6ade9bdbbf54f6de6e8ae08b7ab4
Omar Suleiman <jb18940@bristol.ac.uk>
2021-05-02 15:50:16 +0200

    disabling movement when player jumps on truck

--- Freight/Assets/Scripts/Photon/Map/EndGameSecond.cs
+++ Freight/Assets/Scripts/Photon/Map/EndGameSecond.cs
@@ -66,7 +66,8 @@ public class EndGameSecond : MonoBehaviourPun
         foreach (var player in players)
         {
             player.transform.parent = car.transform;
-            player.GetComponent<PlayerMovementPhoton>().GameEnding();
+            if (player.GetComponent<PlayerMovementPhoton>())
+                player.GetComponent<PlayerMovementPhoton>().GameEnding();
             player.GetComponent<Achievements>().FreightCompleted();
         }
 
