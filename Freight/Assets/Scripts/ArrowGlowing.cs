7e47628976bee2726fc8189b60f13ffffa362b7a
Stefan Pryuna <mc18112@bristol.ac.uk>
2021-05-04 19:11:54 +0300

    Added some random arrows, OMAR needs to do event to destroy all tutorial
    Former-commit-id: 18139ef654471b57a02a3329190245c558a89aa1

---
+++ Freight/Assets/Scripts/ArrowGlowing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowGlowing : MonoBehaviour
+{
+
+    GameObject[] arrows;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrows = GameObject.FindGameObjectsWithTag("Arrow");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        foreach(var arrow in arrows) {
+            if(Vector3.Distance(transform.position, arrow.transform.position) < 25)
+                arrow.GetComponent<Outline>().enabled = true;
+            else
+                arrow.GetComponent<Outline>().enabled = false;
+            
+        }
+    }
+}
