ad5444057fccafba8d139c5ce697e64f9313f978
Stefan Pryuna <mc18112@bristol.ac.uk>
2021-05-10 14:40:14 +0300

    Doors openeing and closing is bugged

---
+++ Freight/Assets/Scripts/Photon/Items/Openable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Openable : Interactable
+{
+   public bool isOpened = false;
+
+   public override void PrimaryInteraction(Character character)
+    {
+        isOpened = !isOpened;
+        if(isOpened == false)
+            character.Open(this);
+        else character.Close(this);
+        
+        
+    }
+}
