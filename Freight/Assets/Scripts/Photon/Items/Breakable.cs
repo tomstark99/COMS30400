a06e9d53b9c96cf41dde24324055c44e2ff45e40
Stefan Pryuna <mc18112@bristol.ac.uk>
2021-05-10 11:18:09 +0300

    Created Breakable class to keep the quality of the code up there

---
+++ Freight/Assets/Scripts/Photon/Items/Breakable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System;
+
+public class Breakable : Interactable
+{
+    public override void PrimaryInteraction(Character character)
+    {
+        character.Break(this);
+    }
+    // Sta
+}
