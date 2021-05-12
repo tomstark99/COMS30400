a0c7b8f51ec7ddbf089ecf411414e39c58d4d2a0
Stefan Pryuna <mc18112@bristol.ac.uk>
2021-05-12 23:38:34 +0300

    arrow destroying is fucccked

---
+++ Freight/Assets/Scripts/DestroyArrows.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class DestroyArrows : MonoBehaviourPun
+{
+    public GameObject arrows;
+
+    void Start() {
+        if(!PhotonView.IsMine)
+            Destroy(this);
+    }
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player") {
+            
+            photonView.RPC(nameof(DeactivateArrowsGlowingRPC), photonView.Owner, other.GetComponent<PhotonView>().ViewID);
+        }
+    }
+
+    void DeactivateArrowsGlowingRPC(int playerID)
+    {
+        Destroy(arrows);
+        PhotonView player = PhotonView.Find(playerID).GetComponent<PhotonView>();
+        Destroy(player.GetComponent<ArrowGlowing>());
+    }
+}
