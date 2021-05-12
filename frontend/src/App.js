6c7b7ec20215d0b6fe071e2469ac00c412e3f04f
Omar Suleiman <jb18940@bristol.ac.uk>
2021-05-12 23:53:28 +0200

    added some bootstrap styling to the navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
+import "react-bootstrap/dist/react-bootstrap.min.js"
 
 const unityContext = new UnityContent(
   'Build/webstef4.json',
@@ -17,9 +19,26 @@ class Game extends React.Component {
   render() {
     return (
       <div>
-        <h1 id="title">Freight</h1>
         <Unity unityContent={unityContext} className="falo" />
       </div>
+    )
+  }
+}
+
+class NavBarGame extends React.Component {
+  render() {
+    return(
+      <Navbar id="navbar-custom" expand="lg">
+        <Navbar.Brand href="/"><span className="nav-text">Freight</span></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/"><span className="nav-text">Game</span></Nav.Link>
+            <Nav.Link href="/about"><span className="nav-text">About</span></Nav.Link>
+            <Nav.Link href="/pose"><span className="nav-text">Pose Controls</span></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     )
   }
 }
@@ -30,19 +49,8 @@ class App extends React.Component {
     return (
       <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+
+        <NavBarGame/>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
@@ -50,8 +58,8 @@ class App extends React.Component {
           <Route path="/about">
             <h1>about</h1>
           </Route>
-          <Route path="/users">
-            <h1>users</h1>
+          <Route path="/pose">
+            <h1>pose</h1>
           </Route>
           <Route path="/">
             <Game />
