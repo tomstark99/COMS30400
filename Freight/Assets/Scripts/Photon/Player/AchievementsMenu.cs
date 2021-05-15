b4d327b2751b7d514e5222711e124cb7d3a249ff
Stefan Pryuna <mc18112@bristol.ac.uk>
2021-05-15 13:06:35 +0300

    Added a 1 at the end of all achievmenets so panel members would get them

--- Freight/Assets/Scripts/Photon/Player/AchievementsMenu.cs
+++ Freight/Assets/Scripts/Photon/Player/AchievementsMenu.cs
@@ -12,52 +12,52 @@ public class AchievementsMenu : MonoBehaviour
         var tempColor = transform.GetChild(0).GetChild(0).GetComponent<Image>().color;
         tempColor.a = 1f;
         // checking for completed achievements
-        if (PlayerPrefs.HasKey("BabySteps"))
+        if (PlayerPrefs.HasKey("BabySteps1"))
         {
             transform.GetChild(0).GetChild(0).GetComponent<Image>().color = tempColor;
         }
-        if (PlayerPrefs.HasKey("LetTheHuntBegin"))
+        if (PlayerPrefs.HasKey("LetTheHuntBegin1"))
         {
             transform.GetChild(0).GetChild(1).GetComponent<Image>().color = tempColor;
         }
-        if (PlayerPrefs.HasKey("PeaceTreaty"))
+        if (PlayerPrefs.HasKey("PeaceTreaty1"))
         {
             transform.GetChild(0).GetChild(2).GetComponent<Image>().color = tempColor;
         }
-        if (PlayerPrefs.HasKey("UseNature"))
+        if (PlayerPrefs.HasKey("UseNature1"))
         {
             transform.GetChild(0).GetChild(3).GetComponent<Image>().color = tempColor;
         }
-        if (PlayerPrefs.HasKey("Roadman"))
+        if (PlayerPrefs.HasKey("Roadman1"))
         {
             transform.GetChild(0).GetChild(4).GetComponent<Image>().color = tempColor;
         }
-        if (PlayerPrefs.HasKey("TheCompletePicture"))
+        if (PlayerPrefs.HasKey("TheCompletePicture1"))
         {
             transform.GetChild(0).GetChild(5).GetComponent<Image>().color = tempColor;
         }
 
-        if (PlayerPrefs.HasKey("LikeANinja"))
+        if (PlayerPrefs.HasKey("LikeANinja1"))
         {
             transform.GetChild(1).GetChild(0).GetComponent<Image>().color = tempColor;
         }
-        if (PlayerPrefs.HasKey("Hackerman"))
+        if (PlayerPrefs.HasKey("Hackerman1"))
         {
             transform.GetChild(1).GetChild(1).GetComponent<Image>().color = tempColor;
         }
-        if (PlayerPrefs.HasKey("LearnTheHardWay"))
+        if (PlayerPrefs.HasKey("LearnTheHardWay1"))
         {
             transform.GetChild(1).GetChild(2).GetComponent<Image>().color = tempColor;
         }
-        if (PlayerPrefs.HasKey("ChooChoo"))
+        if (PlayerPrefs.HasKey("ChooChoo1"))
         {
             transform.GetChild(1).GetChild(3).GetComponent<Image>().color = tempColor;
         }
-        if (PlayerPrefs.HasKey("OnTheRun"))
+        if (PlayerPrefs.HasKey("OnTheRun1"))
         {
             transform.GetChild(1).GetChild(4).GetComponent<Image>().color = tempColor;
         }
-        if (PlayerPrefs.HasKey("Freight"))
+        if (PlayerPrefs.HasKey("Freight1"))
         {
             transform.GetChild(1).GetChild(5).GetComponent<Image>().color = tempColor;
         }
@@ -68,19 +68,19 @@ public class AchievementsMenu : MonoBehaviour
         var tempColor = transform.GetChild(0).GetChild(0).GetComponent<Image>().color;
         tempColor.a = 0.11764705882f;
 
-        PlayerPrefs.DeleteKey("BabySteps");
-        PlayerPrefs.DeleteKey("LetTheHuntBegin");
-        PlayerPrefs.DeleteKey("PeaceTreaty");
-        PlayerPrefs.DeleteKey("UseNature");
-        PlayerPrefs.DeleteKey("Roadman");
-        PlayerPrefs.DeleteKey("TheCompletePicture");
+        PlayerPrefs.DeleteKey("BabySteps1");
+        PlayerPrefs.DeleteKey("LetTheHuntBegin1");
+        PlayerPrefs.DeleteKey("PeaceTreaty1");
+        PlayerPrefs.DeleteKey("UseNature1");
+        PlayerPrefs.DeleteKey("Roadman1");
+        PlayerPrefs.DeleteKey("TheCompletePicture1");
 
-        PlayerPrefs.DeleteKey("LikeANinja");
-        PlayerPrefs.DeleteKey("Hackerman");
-        PlayerPrefs.DeleteKey("LearnTheHardWay");
-        PlayerPrefs.DeleteKey("ChooChoo");
-        PlayerPrefs.DeleteKey("OnTheRun");
-        PlayerPrefs.DeleteKey("Freight");
+        PlayerPrefs.DeleteKey("LikeANinja1");
+        PlayerPrefs.DeleteKey("Hackerman1");
+        PlayerPrefs.DeleteKey("LearnTheHardWay1");
+        PlayerPrefs.DeleteKey("ChooChoo1");
+        PlayerPrefs.DeleteKey("OnTheRun1");
+        PlayerPrefs.DeleteKey("Freight1");
 
         for (int i = 0; i < 2; i++)
         {
