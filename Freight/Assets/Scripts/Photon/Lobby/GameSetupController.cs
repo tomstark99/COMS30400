35011a087152a69c1765ef393ef1639f9738ed23
Omar Suleiman <jb18940@bristol.ac.uk>
2021-05-15 15:33:28 +0200

    added audio source to intro camera
    Former-commit-id: c638ef9f8146d84b4ef9911114b67773695d12d9

--- Freight/Assets/Scripts/Photon/Lobby/GameSetupController.cs
+++ Freight/Assets/Scripts/Photon/Lobby/GameSetupController.cs
@@ -64,6 +64,7 @@ public class GameSetupController : MonoBehaviourPunCallbacks
     {
         cameraObject.GetComponent<Animator>().enabled = true;
         cameraObject.transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
+        cameraObject.GetComponent<AudioSource>().enabled = true;
         guardGameObject.SetActive(true);
         environmentGameObject.SetActive(true);
         trainsGameObject.SetActive(true);
