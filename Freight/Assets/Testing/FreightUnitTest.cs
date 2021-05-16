653b23921e678d4e5e6425c5bc7ca0e9188b44c0
tomstark99 <jb18789@bristol.ac.uk>
2021-05-16 16:42:48 +0100

    Testable stilll

---
+++ Freight/Assets/Testing/FreightUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Photon.Pun;
+
+public class FreightUnitTest : Testable
+{
+    // [SerializeField]
+    // private GameObject guards;
+
+    public override void Test() {
+        TestGuards.Test();
+        Debug.Log("All Tests Passed");
+    }
+
+}
