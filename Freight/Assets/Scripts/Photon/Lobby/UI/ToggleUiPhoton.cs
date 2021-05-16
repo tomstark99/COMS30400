ad82758500a74746ea533afb98cc1c8529154ca7
Omar Suleiman <jb18940@bristol.ac.uk>
2021-05-16 11:21:56 +0200

    pushed new game build for frontend
    Former-commit-id: 253bec65e317fd505a447eb112ebe260e458e594

--- Freight/Assets/Scripts/Photon/Lobby/UI/ToggleUiPhoton.cs
+++ Freight/Assets/Scripts/Photon/Lobby/UI/ToggleUiPhoton.cs
@@ -6,10 +6,7 @@ using TMPro;
 
 public class ToggleUiPhoton : MonoBehaviour
 {
-    public GameObject timetable;
-    public GameObject map;
     public TextMeshProUGUI ping;
-    public GameObject instructions;
     public GameObject pingWarning;
     // Start is called before the first frame update
     void Start()
@@ -23,36 +20,6 @@ public class ToggleUiPhoton : MonoBehaviour
         if (!gameObject.transform.parent.GetComponent<PhotonView>().IsMine)
         {
             return;
-        }
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            if (map.activeSelf || instructions.activeSelf)
-            {
-                map.SetActive(false);
-                instructions.SetActive(false);
-            }
-            timetable.SetActive(!timetable.activeSelf);
-        }
-
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            if (timetable.activeSelf || instructions.activeSelf)
-            {
-                timetable.SetActive(false);
-                instructions.SetActive(false);
-            }
-            map.SetActive(!map.activeSelf);
-        }
-
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            if (map.activeSelf || timetable.activeSelf)
-            {
-                map.SetActive(false);
-                timetable.SetActive(false);
-            }
-            instructions.SetActive(!instructions.activeSelf);
         }
 
         int pingVal = PhotonNetwork.GetPing();
