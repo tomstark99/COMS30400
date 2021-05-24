ad4097e8962031197f7baae0872bdb7ac6746b11
Omar Suleiman <jb18940@bristol.ac.uk>
2021-05-24 15:08:27 +0200

    think we commented all scripts we worked on?

--- Freight/Assets/Scripts/Photon/Player/Minimap.cs
+++ Freight/Assets/Scripts/Photon/Player/Minimap.cs
@@ -6,19 +6,19 @@ using UnityEngine;
 public class Minimap : MonoBehaviour
 {
     private Transform player;
-    // private GameObject[] bags;
-    // private GameObject[] players;
 
-    void Start() {
+
+    void Start() 
+    {
         player = transform.parent;
     }
-    // Start is called before the first frame update
-    void Update() {
+    
+    void Update() 
+    {
         Vector3 newPos = player.position;
         newPos.y = transform.position.y;
         transform.position = newPos;
         transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0.0f);
     }
-
    
 }
