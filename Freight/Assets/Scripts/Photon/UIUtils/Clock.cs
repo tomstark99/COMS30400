bfa6d0a048ca96f8c75cc4319334f1657053839c
Omar Suleiman <jb18940@bristol.ac.uk>
2021-05-24 21:37:00 +0200

    quiick ting

--- Freight/Assets/Scripts/Photon/UIUtils/Clock.cs
+++ Freight/Assets/Scripts/Photon/UIUtils/Clock.cs
@@ -33,10 +33,10 @@ public class Clock : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //startTime = DateTime.Now;
-        //InvokeRepeating("updateClock", 1f, 1f);
+       
         GameObject.FindGameObjectWithTag("EndGame").GetComponent<EndGame>().EndTheGame += ClearClock;
 
+        // if client is master client, we take the server time of the PhotonNetwork and set it to be StartTime in the custom room properties
         if (PhotonNetwork.IsMasterClient)
         {
             CustomValue = new ExitGames.Client.Photon.Hashtable();
@@ -45,6 +45,7 @@ public class Clock : MonoBehaviour
             CustomValue.Add("StartTime", startTime);
             PhotonNetwork.CurrentRoom.SetCustomProperties(CustomValue);
         }
+        // if client is second client, we check if this value has been set already and get the time the master client set, to synchronise 
         else
         {
             if (PhotonNetwork.CurrentRoom.CustomProperties["StartTime"] != null)
@@ -65,7 +66,9 @@ public class Clock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!startTimer) {
+        // we run this in update in case the StartTime was not set in Start, so we keep checking until we can set it to something
+        if (!startTimer) 
+        {
             if (PhotonNetwork.CurrentRoom.CustomProperties["StartTime"] != null)
             {
                 startTime = double.Parse(PhotonNetwork.CurrentRoom.CustomProperties["StartTime"].ToString());
@@ -75,22 +78,34 @@ public class Clock : MonoBehaviour
             {
                 return;
             }
-        } else if (!trainLeft)
+        } 
+        // if train has not yet left, we keep decrementing the counter
+        else if (!trainLeft)
         {
+            // to get synchronised time between clients we take the time to leave from game settings and add it to the initial PhotonNetwork time set by master client subtracted from the 
+            // current PhotonNetwork time. 
+            // this method ensures both clients have the exact same time displayed 
             double newTime = timeToLeave + (startTime - PhotonNetwork.Time);
             timerIncrementer = Math.Round(newTime, 4);
-            text.text = "Time to leave: " + FormatTime(timerIncrementer);//timerIncrementer.ToString();
-            if(Math.Round(newTime) <= 60) {
+            text.text = "Time to leave: " + FormatTime(timerIncrementer);
+            if(Math.Round(newTime) <= 60) 
+            {
                 bool flashing = animator.GetBool(flashHash);
+                // on 30, 10, 5 seconds we have a short flashing animation
                 if ((Mathf.Ceil((float) newTime) % 30 == 0 && !flashing) || 
                     (Mathf.Ceil((float) newTime) == 10 && !flashing) || 
-                    (Mathf.Ceil((float) newTime) == 5 && !flashing)) {
+                    (Mathf.Ceil((float) newTime) == 5 && !flashing)) 
+                {
                     animator.SetBool(flashHash, true);
-                } else if (flashing) {
+                } 
+                // if previously was flashing, we set to not flashing
+                else if (flashing) 
+                {
                     animator.SetBool(flashHash,false);
                 }
                 text.fontSize = Mathf.Lerp(16,20,t);
                 if (t <= 1) t += Time.deltaTime/60.0f;
+                // if time reaches 0, we set train is leaving message
                 if (newTime <= 0)
                 {
                     trainLeft = true;
@@ -101,28 +116,12 @@ public class Clock : MonoBehaviour
         } 
     }
 
-    private string FormatTime( double time ) {
+    private string FormatTime( double time ) 
+    {
         int min = (int) time / 60;
         int sec = (int) time - 60 * min;
         int mil = (int) (1000 * (time - min * 60 - sec));
         return time < 60 ? string.Format("{0:00}:{1:00}:{2:00}", min, sec, Mathf.Floor(mil / 10)) : string.Format("{0:00}:{1:00}", min, sec);
-    }
-
-    //void updateClock()
-    //{
-    //    currentTime = DateTime.Now;
-    //    TimeSpan difference = currentTime - startTime;
-    //    gameTime = Convert.ToDateTime("01/01/2020 12:00:00");
-    //    gameTime = gameTime.Add(difference);
-    //    hour = leadingZero(gameTime.Hour);
-    //    minute = leadingZero(gameTime.Minute);
-    //    second = leadingZero(gameTime.Second);
-    //    text.text = hour + ":" + minute + ":" + second;
-    //}
-
-    string leadingZero(int n)
-    {
-        return n.ToString().PadLeft(2, '0');
     }
 
     void ClearClock()
